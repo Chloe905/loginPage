aed196e669b92b5b8683a8da35e1bd089dbe4dcb
Chloe905 <cchloehsieh@gmail.com>
2022-11-26 22:13:55 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const PORT = 3000
+
+mongoose.connect(process.env.MONGODB_URI) //資料庫連線
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => {
+  console.log('mongodb connect error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 const app = express()
 
