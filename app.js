c0d2890f477a3f7b6a79f160e20e73fd10c36a7e
Chloe905 <cchloehsieh@gmail.com>
2022-11-29 22:04:28 +0800

    feat: add login function and welcome page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const ephbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const Login = require('./models/login')
 const PORT = 3000
 
 mongoose.connect(process.env.MONGODB_URI) //資料庫連線
@@ -19,9 +21,25 @@ db.once('open', () => {
 const app = express()
 app.engine('handlebars', ephbs.engine({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('login')
+})
+
+app.post('/', (req, res) => {
+  const inputEmail = req.body.inputEmail
+  const inputPassword = req.body.inputPassword
+  Login.findOne({ email: inputEmail, password: inputPassword })
+    .lean()
+    .then((user) => {
+      if (!user) {
+
+        res.redirect('/')
+      } else {
+        res.render('welcome', { firstName: user.firstName })
+      }
+    })
 })
 
 app.listen(PORT, () => {
