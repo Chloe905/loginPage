b59f74e5d8e9660160cb7a102010ea997ab37230
Chloe905 <cchloehsieh@gmail.com>
2022-12-05 19:37:51 +0800

    feat: add session-cookie function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ephbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -22,24 +23,43 @@ const app = express()
 app.engine('handlebars', ephbs.engine({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'My secret', // 存放在cookie的sessionID
+  name: 'user', // optional ，預設為connect.sid
+  // 代表在每次與使用者互動後，不會強制把 session 儲存，除非 session 有變動
+  resave: false,
+  saveUninitialized: false
+}))
 
 app.get('/', (req, res) => {
-  res.render('login')
+  if (req.session.isLogin) {
+    res.render('welcome', { firstName: req.session.user })
+  } else {
+    res.render('login')
+  }
 })
 
 app.post('/', (req, res) => {
-  const inputEmail = req.body.inputEmail
-  const inputPassword = req.body.inputPassword
+  const { inputEmail, inputPassword } = req.body
   Login.findOne({ email: inputEmail, password: inputPassword })
     .lean()
     .then((user) => {
       if (!user) {
-
-        res.redirect('/')
+        // 輸入錯誤顯示錯誤訊息及原本輸入的顯示於輸入框
+        const errMessage = 'Wrong Email or Password!'
+        res.render('login', { email: inputEmail, password: inputPassword, errMessage })
       } else {
+        req.session.user = user.firstName
+        req.session.isLogin = true
         res.render('welcome', { firstName: user.firstName })
+        console.log('sessionID:' + req.sessionID)
       }
     })
+})
+// 加入homepage 驗證sessionID
+app.get('/home', (req, res) => {
+  console.log('sessionID:' + req.sessionID)
+  res.send('home page')
 })
 
 app.listen(PORT, () => {
